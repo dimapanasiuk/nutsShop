947b3ef7f3b1253afb112d290eccb95ddd1c6579
dimapanasiuk <dimapanasiuk@icloud.com>
2020-07-14 19:43:02 +0300

    add all languages for pop up

--- js/myJs/popupContent.mjs
+++ js/myJs/popupContent.mjs
@@ -4,8 +4,16 @@ let delivery = {
       <h2>Доставка</h2>
       <p>Специалисты нашего логистического отдела готовы организовать доставку Вашего заказа в любую точку Европы.</p>
     </div>`,
-    en: `<h1>delivery</h1>`,
-    pl: `<h1>delivery</h1>`,
+    en: `
+    <div id='content'>
+      <h2>Delivery</h2>
+      <p>Specialists of our logistics department are ready to organize the delivery of your order to any point of Europe.</p>
+    </div>`,
+    pl: `
+    <div id='content'>
+      <h2>Dostawa</h2>
+      <p>Specjaliści z naszego działu logistyki są gotowi zorganizować dostawę Twojego zamówienia do dowolnego miejsca w Europie.</p>
+    </div>`
 };
 
 let productionAria = {
@@ -18,8 +26,24 @@ let productionAria = {
         заготовителями в Республиках Бурятия и Хакасия, Томск
       </p>
     </div>`,
-    en: `<h1>productionAria</h1>`,
-    pl: `<h1>productionAria</h1>`,
+    en: `
+    <div id='content'>
+      <h2>The harvest area</h2>
+      <p>
+        We process pine nut kernels, berries and mushrooms harvested in the ecologically clean regions of Siberia.
+        Every year we open our purchase centers in the Mountain Altai region and Altai Krai, and cooperate with
+        suppliers from Buryatia and Khakassia, Tomsk region.
+      </p>
+    </div>`,
+    pl: `
+    <div id='content'>
+      <h2>Obszar żniw</h2>
+      <p>
+        Przetwarzamy ziarna orzechów piniowych, jagody i grzyby zebrane w ekologicznie czystych regionach Syberii.
+        Każdego roku otwieramy nasze centra zakupowe w regionie Gór Ałtaju i Ałtajskim Kraju oraz współpracujemy
+        z dostawcami z Buriacji i Chakasji, Tomska.
+      </p>
+    </div>`
 };
 
 let production = {
@@ -32,9 +56,26 @@ let production = {
         качества ISO 9001-2011 и система менеджмента безопасности продуктов питания ISO 22000-2007.
       </p>
     </div>`,
-    en: `<h1>production</h1>`,
-    pl: `<h1>production</h1>`,
+    en: `
+    <div id='content'>
+      <h2>Production</h2>
+      <p>  
+        The factory is equipped with the best Russian and Italian machines. At the beginning of 2015, the factory was certified
+        for compliance with all regulations and standards of HACCP. Earlier, the factory introduced the quality management system ISO 
+        9001-2011 and the food safety management system ISO 22000-2007.
+      </p>
+    </div>`,
+    pl: `
+    <div id='content'>
+      <h2>Produkcja</h2>
+      <p>  
+        Fabryka jest wyposażona w najlepsze maszyny rosyjskie i włoskie. Na początku 2015 r. Fabryka otrzymała certyfikat zgodności 
+        ze wszystkimi przepisami i standardami HACCP. Wcześniej fabryka wprowadziła system zarządzania jakością ISO 9001-2011 i system
+        zarządzania bezpieczeństwem żywności ISO 22000-2007.  
+      </p>
+    </div>`,
 };
+
 let quality = {
     ru: `
     <div id='content'>
@@ -45,54 +86,84 @@ let quality = {
         SEA PIXEL и ручного контроля органолептических показателей контрольного образца.
       </p>
     </div>`,
-    en: `<h1>quality</h1>`,
-    pl: `<h1>quality</h1>`
+    en: `
+    <div id='content'>
+      <h2>Quality control</h2>
+      <p>  
+      Each batch passes frequent quality control at all stages of production: size control, dual humidity control, quality control 
+      of the kernel purification. The final stage of quality control, we carry out with the help of the Italian separator SEA PIXEL
+      and manual control of the organoleptic indicators of check sample</p>
+    </div>`,
+    pl: `
+    <div id='content'>
+      <h2>Kontrola jakości</h2>
+      <p>  
+        Każda partia przechodzi częstą kontrolę jakości na wszystkich etapach produkcji: kontrola wielkości, podwójna kontrola wilgotności,
+        kontrola jakości oczyszczania jądra. Ostatni etap kontroli jakości przeprowadzamy za pomocą włoskiego separatora SEA PIXEL i ręcznej 
+        kontroli wskaźników organoleptycznych próbki kontrolnej.  
+      </p>
+    </div>`
 };
 
 let laboratory = {
-    ru: `<div id='content'>
-  <h2>Лабораторные исследования</h2>
-  <p>  
-    Каждая партия готовой продукции подвергается тщательным микробиологическим исследованиям (БГКП, патогенные микроорганизмы, плесени).
-    Также мы проводим дополнительные исследования на радионуклиды и афлатоксины. Полученные результаты вносятся в паспорт качества продукции.
-  </p>
-</div>`,
-    en: `<h1>EN laboratory</h1>`,
-    pl: `<h1>pl laboratory</h1>`
+    ru: `
+    <div id='content'>
+      <h2>Лабораторные исследования</h2>
+      <p>  
+        Каждая партия готовой продукции подвергается тщательным микробиологическим исследованиям (БГКП, патогенные микроорганизмы, плесени).
+        Также мы проводим дополнительные исследования на радионуклиды и афлатоксины. Полученные результаты вносятся в паспорт качества продукции.
+      </p>
+    </div>`,
+    en: `
+    <div id='content'>
+      <h2>Laboratory research</h2>
+      <p>  
+        Every batch undergoes microbiological control. Compliance with the following indexes is checked: BGKP, pathogenic microorganisms, different kinds
+        of mold. We also conduct additional control of radionuclides and aflatoxins. The results received are included into the Quality Passport.
+      </p>
+    </div>`,
+    pl: `
+    <div id='content'>
+      <h2>Badania laboratoryjne</h2>
+      <p>  
+        Każda partia podlega kontroli mikrobiologicznej. Sprawdzana jest zgodność z następującymi indeksami: BGKP, mikroorganizmy chorobotwórcze, różne rodzaje pleśni.
+        Prowadzimy również dodatkową kontrolę radionuklidów i aflatoksyn. Otrzymane wyniki są zawarte w paszporcie jakości. 
+      </p>
+    </div>`
 };
 
-let productContentOne = {
-    tabeHeaderTittleFirst: "Пищевая ценность, 100г:",
-    tabeHeaderTittleSecond: "эн. ценн. 875 ккал",
-    tabelItemContentFirst: "белки 13,7 г",
-    tabelItemContentSecond: "жиры 68,4 г",
-    tabelItemContentThird: "углеводы 13,7 г",
-    aboutFirst: "100 граммов орехов на треть удовлетворяют суточную потребность человеческого организма в белке.",
-    aboutSecond: `Вода — 2,3 г; Тиамин (B1) — 0,4 мг; Рибофлавин (B2) — 0,2 мг; Аскорбиновая кислота (вит. С) — 0,8 мг;
-     Витамин K — 53,9 мкг; Кальций — 16 мг; Железо — 5,5 мг; Магний — 251 мг; Фосфор — 575 мг; Калий — 597 мг; Цинк — 6,4 мг.`,
-};
+// let productContentOne = {
+//     tabeHeaderTittleFirst: "Пищевая ценность, 100г:",
+//     tabeHeaderTittleSecond: "эн. ценн. 875 ккал",
+//     tabelItemContentFirst: "белки 13,7 г",
+//     tabelItemContentSecond: "жиры 68,4 г",
+//     tabelItemContentThird: "углеводы 13,7 г",
+//     aboutFirst: "100 граммов орехов на треть удовлетворяют суточную потребность человеческого организма в белке.",
+//     aboutSecond: `Вода — 2,3 г; Тиамин (B1) — 0,4 мг; Рибофлавин (B2) — 0,2 мг; Аскорбиновая кислота (вит. С) — 0,8 мг;
+//      Витамин K — 53,9 мкг; Кальций — 16 мг; Железо — 5,5 мг; Магний — 251 мг; Фосфор — 575 мг; Калий — 597 мг; Цинк — 6,4 мг.`,
+// };
 
-let productContent = (content) => {
-    return `
-  <div id='content'>
-    <ul class="table-header">
-      <li class="table-header_item">${content.tabeHeaderTittleFirst}</li>
-      <li class="table-header_item table-header_last-item">${content.tabeHeaderTittleSecond}</li>
-    </ul>
-    <ul class="table-content">
-      <li class="table-content_item">${content.tabelItemContentFirst}</li>
-      <li class="table-content_item">${content.tabelItemContentSecond}</li>
-      <li class="table-content_item table-content_last-item">${content.tabelItemContentThird}</li>
-    </ul>
-    <p>
-      ${content.aboutFirst}
-    </p>
-    <p> 
-      ${content.aboutSecond}
-    </p>
-</di>
-`;
-};
+// let productContent = (content) => {
+//     return `
+//   <div id='content'>
+//     <ul class="table-header">
+//       <li class="table-header_item">${content.tabeHeaderTittleFirst}</li>
+//       <li class="table-header_item table-header_last-item">${content.tabeHeaderTittleSecond}</li>
+//     </ul>
+//     <ul class="table-content">
+//       <li class="table-content_item">${content.tabelItemContentFirst}</li>
+//       <li class="table-content_item">${content.tabelItemContentSecond}</li>
+//       <li class="table-content_item table-content_last-item">${content.tabelItemContentThird}</li>
+//     </ul>
+//     <p>
+//       ${content.aboutFirst}
+//     </p>
+//     <p> 
+//       ${content.aboutSecond}
+//     </p>
+// </di>
+// `;
+// };
 
 export {
     delivery,
@@ -100,6 +171,6 @@ export {
     production,
     quality,
     laboratory,
-    productContent,
-    productContentOne,
+    // productContent,
+    // productContentOne,
 };
